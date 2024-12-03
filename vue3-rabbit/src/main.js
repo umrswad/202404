ac8c5b63d1356464107420a6120715a4b60b8d27
烈红色 <2250652265@qq.com>
2024-12-03 16:06:48 +0800

    Update main.js

--- vue3-rabbit/src/main.js
+++ vue3-rabbit/src/main.js
@@ -6,7 +6,7 @@ import router from "./router";
 // 全局指令注册
 import { lazyPlugin } from "@/directives";
 import "@/styles/common.scss";
-// 引入全局组件插件
+// 引入全局组件插件11
 import { componentPlugin } from "@/components";
 
 const app = createApp(App);
